20ff6aca4098fea9317821f4632efe73e6dda70d
Marufomer <omermaruf07@gmail.com>
2024-10-03 02:34:05 -0700

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,23 +2,24 @@
 """
 Pascal's Triangle
 """
-from math import factorial;
+from math import factorial
 
 
-def pascal_triangle(rows):
+def pascal_triangle(n):
     """
     Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n
     """
 
     result = [] # collection of arry
-    if rows > 0: # Returns an empty list if n <= 0
+    if n > 0: # Returns an empty list if n <= 0
         def combine(n, r):
             return int((factorial(n))/(factorial(r)*factorial(n-r))) # calculate element of row
 
-        for i in range(rows):
+        for i in range(n):
             row = [] # element of row 
 
             for j in range(i + 1):
                 row.append(combine(i,j))
             result.append(row)
     return result
+
